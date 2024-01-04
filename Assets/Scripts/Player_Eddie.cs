3e2180bb6c00a872b491d7a5b5aec717747e165c
Teddie Eddie <coffeeteddie@gmail.com>
2024-01-04 10:08:27 -0800

    Player Health Bar & My Script
    I managed to create a health bar that will decrease based on damage. It still needs to implement enemy collision, specific enemy damage and armor so it's done yet. I also added my own copy of Alex's "Player" script called "Player_Eddie." Please no touch :3

---
+++ Assets/Scripts/Player_Eddie.cs
@@ -0,0 +1,60 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.PlasticSCM.Editor.WebApi;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Player_Eddie : MonoBehaviour
+{
+
+    //Player Object
+    public GameObject player;
+
+
+    //Player Movement Variables
+    public float moveSpeed = 5f;
+    public Rigidbody2D rb;
+    Vector2 movement;
+
+
+    //Player Health Variables
+    public int maxHealth = 100;
+    public int currentHealth;
+    public Health health;
+
+    void Start()
+    {
+        //Start Health = 100 at Game Start
+        currentHealth = maxHealth;
+        health.setMaxHealth(maxHealth);
+    }
+
+    void Update()
+    {
+        //Movement Keys
+        movement.x = Input.GetAxisRaw("Horizontal");
+        movement.y = Input.GetAxisRaw("Vertical");
+
+
+
+        if(Input.GetKeyDown(KeyCode.Space)) {
+            Damage(10);
+        }
+        
+    }
+
+    void FixedUpdate()
+    {
+        //Rigid Body Position Speed
+        rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
+    }
+
+
+    void Damage(int damage) {
+
+        //Set Health Int and HealthBar
+        currentHealth -= damage;
+        health.setHealth(currentHealth);
+
+    }
+}
